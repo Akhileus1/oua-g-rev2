f7cfbb6943a12f3a3a17eb361cde691509969ba0
Akhileus1 <eren.alp13389@gmail.com>
2023-03-24 17:38:55 +0300

    ilk branch oluşturdum

---
+++ eren.java
@@ -0,0 +1,134 @@
+public class  main {
+	
+	 public static void main(String[] args) {
+		System.out.println("Merhabalar");
+
+		System.out.println("I am learning java");
+		System.out.println("It is awesome!!");
+
+		System.out.println(3);
+		System.out.println(358);
+		System.out.println(50000);
+
+		System.out.println(3+3);
+		System.out.println(2*5);
+
+		// this is a comment
+
+		System.out.println("Merhabalar");
+
+
+		/* the code below will print the words hello world to the screen,and  it is amazing*/
+
+		System.out.println("Merhabalar");
+
+		String name ="Hector";
+		System.out.println(name);
+
+		int mynum = 24;
+		System.out.println(mynum);
+
+		int myNum =16;
+		myNum = 18; // myNum is now 18
+		System.out.println(myNum);
+
+		final int mymum = 19;
+		mymum = 20; // will generate an error ; cannot assing a vaule to a final variable
+
+
+		int hep =6;
+		float myfunc =5.99f;
+		char mychar = 'H';
+
+		boolean mybool = false;
+
+		String mytext ="hello";
+
+		String name1 = "Helen";
+		System.out.println("hello" + name1);
+
+		String fristname = " john" ;
+		String lastname = "doe";
+		String fullname = fristname +lastname;
+		System.out.println(fullname);
+
+		int x =10;
+		int y= 5;
+		System.out.println(x+y); //Print the vaule of x+y
+
+		int a = 5;
+		int b= 6;
+		int c =7;
+		System.out.println(a+b+c);
+
+		int k,l,m;
+
+		k=l=m= 100;
+		System.out.println(k+l+m);
+
+		// Good
+
+		int minutesperhour =80;
+
+		// OK, but not so easy to understand what m actally is 
+		int m =90;
+
+		byte myhey =100;
+		System.out.println(mhey);
+
+
+		boolean isJavaFun = true;
+		boolean isFinishTasty = false;
+
+		System.out.println(isJavaFun); //outputs true
+		System.out.println(isFinishTasty); // outputs false
+
+		char mygrade = 'B';
+
+		System.out.println(mygrade);
+
+		char myVar1 =65 , myvar2=67 ,myvar3=68 ;
+
+		System.out.println(myVar1);
+		System.out.println(myvar2);
+		System.out.println(myvar3);
+
+		String good = "hello";
+
+		System.out.println(good);
+
+		String greeting ="selam";
+		System.out.println(greeting);
+	}
+}
+
+public class public static void main(String[] args) {
+	int het = 9;
+	double myDuble =myInt;  // Automatic casting: intto double
+
+	System.out.println(myInt); // Outputs 9
+	System.out.println(myDuble); // Outputs 9.0
+
+}
+
+public class public static void main(String[] args) {
+	double mydouble = 9.78d;
+
+	int myint = (int) mydouble; // manuel casting: double to int
+
+}a
+
+class  public static void main(String[] args) {
+	int x= 100+ 50;
+
+	int sum1 =100+50; //150
+	int sum2 = sum1+ 255; //405
+	int sum3 =sum2 *sum2 ; //405x405
+
+	int x =10 ;
+	x += 5;
+
+	
+
+
+}a
